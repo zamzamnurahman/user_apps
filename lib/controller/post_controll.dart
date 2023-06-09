a7182d55dd877fc4ab0e6b62f0d4d8728e43fa88
syfa29faa <2006133@itg.ac.id>
2023-06-09 11:46:22 +0700

    mengubah post controll

--- lib/controller/post_controll.dart
+++ lib/controller/post_controll.dart
@@ -6,32 +6,11 @@ import 'package:user_apps/model/post.dart';
 
 import '../config/constant.dart';
 
-class UserController {
+class PostController {
   Future<List<Post>> getPostData() async {
     Uri url = Uri.parse(ApiConstant.baseUrl + ApiConstant.postEndpoint);
-
-    try {
-      Response response = await http.get(url);
-      // List<Map<String, dynamic>> posts = await getPosts();
-      List dataDecode = jsonDecode(response.body);
-      for (int i = 0; i < dataDecode.length; i++) {
-        dataDecode[i]['photo'] = "assets/user_img/user${i + 1}.jpg";
-      }
-
-      List<Post> result =
-          dataDecode.map((post) => Post.fromJson(post)).toList();
-
-      // for (Map<String, dynamic> post in posts) {
-      //   for (User user in result) {
-      //     if (post['userId'] == user.id) {
-      //       user.post?.add(Post.fromJson(post));
-      //     }
-      //   }
-      // }
-
       return result;
     } catch (error) {
       throw Exception(error);
     }
   }
-}
